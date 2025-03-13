3a843fba2df07406638cc3fb9d7dfab7be68f8a6
Mohammad yousefi <mamad12sale@gmail.com>
2025-03-13 21:46:17 +0330

    Debug

--- DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/obj/Debug/net8.0/DataType_cs.AssemblyInfo.cs
+++ DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/obj/Debug/net8.0/DataType_cs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03ecba7624e047e5379310a5fe9ceaa9ad06f713")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b25c3a45a3b50aba8d3232d1abbb54323c6d6c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
