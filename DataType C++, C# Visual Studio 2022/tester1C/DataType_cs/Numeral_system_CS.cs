7d505d9728a890831329f0b1ef157997b326782c
Mohammad yousefi <mamad12sale@gmail.com>
2025-03-13 15:11:29 +0330

    Aded Numeral_System in C#

---
+++ DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/Numeral_system_CS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType_cs
+{
+    class Numeral_system_CS
+    {
+        static void Main(string[] numeral)
+        {
+
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
