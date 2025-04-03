303084f47e78af6067f9a68fea58897f12babfed
Mohammad yousefi <mamad12sale@gmail.com>
2025-04-03 20:47:02 +0330

    Writing code Unary_Arithmetic_Operators in MSVC

---
+++ DataType C++, C# Visual Studio 2022/tester1C/tester1C/Unary_Arithmetic_Operators.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+// Dar Unary operator kolan 1 Operand darim
+// Va 2 ta operator
+// Minus --> - 
+// Plus --> +
+
+int main() {
+
+	// Mesal minus: 
+	int x = 5;
+	// Manfi dar mosbat mishe -
+	std::cout << "Minus: " << -x << '\n';
+	int y = -5;
+	// Manfi dar manfi mishe +
+	std::cout << "Minus: " << -y << '\n'; // معکوس میکنه
+	
+	// Mesal plus:
+	// Mosbat dar mosbat mishe +
+	std::cout << "Plus: " << +x << '\n';
+	// Manfi dar mosbat mishe -
+	std::cout << "Plus: " << +y << '\n'; 
+	
+	// Nokte: In 2 ta operator ke neveshtim Minus va Plus 
+	// Hich rabti be Operator haye Addition va Subtraction nadarand
+	// Chon Addition va Subtraction 2 ta amalvand darand
+	// x + y && x - y
+	// Dar Binary_Arithmetic_Operators goftam
+
+}
