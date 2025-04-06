26ae606344e162e7ce2e8385c2934c2b121a256a
Mohammad yousefi <mamad12sale@gmail.com>
2025-04-07 01:00:47 +0330

    Writing code Operators Precedence with C++ in VSCode

--- Season4.cpp
+++ Season4.cpp
@@ -32,7 +32,48 @@
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+// Mabhas: اولویت عملگر ها
 
+// Khob in bebin --> 2 + 4 x 3
+// Be inayi bishtar az 1 Operator darand migan:
+// Compound Expression
 
+// Baraye arzyabi Compound Expression bayad avval bedunim 
+// Har kodum az in Operator ha che kari anjam midahand
+// Va hatman bayad bedunim اولویت kodum Operator balatar ast
 
+// اولویت یعنی چه؟
+// Yani bayad bedunim avval kodum Operator mohasebe mishe
+// Be اولویت moshkhas mishe kodum Operator bayad avval arzyabi beshe
+// Migan: Operators Precedence --> اولویت عملگر ها
 
+// Be tartibi ke moshkhas mikone kodum Operator ghabl az on Operator arzyabi
+// Beshe migan --> Operators Precedence
+
+// Dar in ebarati ke neveshtim 2 + 4 x 3
+// اولویت avval Operator x hast
+// Vali age avval + she ba'ad x she Output fargh mikone
+
+// Rah hal:
+// Hala man barname ke neveshtam khastam olaviyat avval + bashe chetori
+// Estefade az Parantez ( ) Olaviyat avval ro dare
+// Mesal:
+/*
+#include <iostream>
+
+int main() {
+
+    std::cout << "Compound Expression: " << 2 + 4 * 3 << '\n';
+    std::cout << "Compound Expression: " << (2 + 4) * 3 << '\n';
+    std::cout << "Compound Expression: " << 2 + (4 * 3) << '\n';
+    // In code akhari che parantez bezari che nazari olaviyat * hast
+    // Vali tosiye mishe Parantez bezarim !!!
+    // Age niyaz ham nabod baz Parantez tosiye mishe khanayi bala tar code !!
+
+    return 0;
+}
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+// Mabhas: عملگر محاسباتی
