d1123e1248ad15fd63a806f92d76def211f1e0eb
Mohammad yousefi <mamad12sale@gmail.com>
2025-04-07 00:35:18 +0330

    Writing Introduction Season4 in VSCode with C++

--- Season4.cpp
+++ Season4.cpp
@@ -3,3 +3,36 @@
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
+
+// Moghadame:
+// Dar in fasl mikhaym be 2 موضوع bepardazim
+// 1-Operators 
+// 2-Bit Manipulation --> دست کاری بیت
+
+// Dar avakhare in season mipardazim be nahve zakhire sazi adad manfi dar hafeze
+
+
+//-------------------//
+
+// Khob dar riyaziyat ye mafhumi darim be name:
+// Operation --> عملیات
+
+
+// Mesal: 
+// In 5 + 3 yek Operation hast
+// Va har yek Operation mitune bishtar az 1 Operand va Operator dashte bashe
+
+// Operand chiye? عملوند
+// Hamin 5 + 3 in adad 5 va 3 Operand hesab mishan
+
+// Har Operation ham yek Output tolid mikone 5 + 3 = 8 Result mishe 8
+// Chetori in Output be dast omad hamin adad 8?
+// Ba yek namad ke migim Operator --> + 
+// Ba estefade az Operator ha mitunim khoruji tolid konim
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
+
