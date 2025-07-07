a3bb46a6698173230229fb7d716ede1c7641771e
Mohammad yousefi <mohamadexbier@gmail.com>
2025-07-07 22:03:32 +0330

    Writing Description about Adding_in_Binary in Visual Studio 2022

---
+++ DataType C++, C# Visual Studio 2022/tester1C/tester1C/Adding_in_Binary.cpp
@@ -0,0 +1,24 @@
+// Khob mikham jam' adad binary ro yad begirim
+// Kheyli rahate
+// 0 + 0 = 0
+// 1 + 0 = 1
+// 0 + 1 = 1
+// 1 + 1 = 0 yedune 1 midim be sotun ba'adi yani samt chap
+// 1 + 1 + 1 = 1 yedune 1 midim be sotun ba'adi yani samt chap
+
+// Kharej az in 5 halat nadarim
+// Ye mesal: 4 + 3 = 0100 + 0011
+// 4:   0100
+// 3:   0011
+// Out: 0111 --> 7
+
+// Ye mesal dige: 10 + 2 = 1010 + 0010
+//		(1)		
+// 10:  1010
+//  2:  0010
+// Out: 1100 --> 12
+
+// Biaym be yek adad 1 vahed ezafe konim: 8 + 1 = 1000 + 0001
+// 8:   1000
+// 1:   0001
+// Out: 1001 --> 9
