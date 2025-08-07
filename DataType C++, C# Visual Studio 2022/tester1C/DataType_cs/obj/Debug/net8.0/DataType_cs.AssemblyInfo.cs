7e41a18b054ff430d2d846c19c5eeb94daeefe91
PyScriptPlus <mohamadexbier@gmail.com>
2025-08-08 02:35:08 +0330

    Update ziad mohem nist

--- DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/obj/Debug/net8.0/DataType_cs.AssemblyInfo.cs
+++ DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/obj/Debug/net8.0/DataType_cs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1756603a44760934fc52632ddb5ff4bdb7fa85ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7861b78b5884e4e5f7c68bc602242b86a6a6fa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
