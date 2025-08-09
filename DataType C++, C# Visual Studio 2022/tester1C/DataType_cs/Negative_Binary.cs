0ba4d82cc7c54fd24a544a9b8643154beef6536d
PyScriptPlus <mohamadexbier@gmail.com>
2025-08-09 18:38:57 +0330

    Writing code about Negative_Binary in Memory With C#

---
+++ DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/Negative_Binary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType_cs
+{
+    internal class Negative_Binary
+    {
+        static void Main(string[] args)
+        {
+            // Unsigned int8: byte
+            // Signed int8: sbyte
+            byte seven = 0b0000_0111;
+            seven = (byte)~seven;
+            sbyte sevenAdd = (sbyte)(seven + 0b0000_0001);
+            Console.WriteLine(sevenAdd); // -7 
+
+        }
+    }
+}
