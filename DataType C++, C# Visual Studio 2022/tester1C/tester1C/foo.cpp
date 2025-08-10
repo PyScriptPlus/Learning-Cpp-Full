ea3a55ccb8e891ce1d5e79e33f46a421afab41ca
PyScriptPlus <mohamadexbier@gmail.com>
2025-08-11 00:44:32 +0330

    Writing code about Namespace & Collision_Name with C++ in MSVC

---
+++ DataType C++, C# Visual Studio 2022/tester1C/tester1C/foo.cpp
@@ -0,0 +1,14 @@
+/* Error Duplicate
+int doSomething(int x, int y) {
+	return x + y;
+}
+*/
+
+// Rah hal: namespace
+// user-defined-namespace:
+namespace foo {
+	int doSomething(int x, int y) {
+		return x + y;
+	}
+}
+
---
+++ DataType C++, C# Visual Studio 2022/tester1C/tester1C/goo.cpp
@@ -0,0 +1,14 @@
+/* Error Duplicate
+int doSomething(int x, int y) {
+	return x - y;
+}
+*/
+
+// Rah hal: namespace
+// user-defined-namespace:
+namespace goo {
+	int doSomething(int x, int y) {
+		return x - y;
+	}
+}
+
