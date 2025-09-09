4b9a8bfee7efa100a0762d6da7e276f82af11ca6
PyScriptPlus <mohamadexbier@gmail.com>
2025-09-09 19:44:23 +0330

    Writing code about IEnumrator with C#

---
+++ Learning_CSharp_Full/enumrator_and_enumrable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Learning_CSharp_Full
+{
+    internal class enumrator_and_enumrable
+    {
+        static void Main()
+        {
+            string[] names = { "Ali", "Hassan", "Danial" };
+            IEnumerator enumerator = names.GetEnumerator();
+
+            enumerator.Reset();
+            enumerator.MoveNext();
+            while (enumerator.MoveNext())
+            {
+                string name = (string)enumerator.Current;
+                Console.WriteLine(name);
+                //enumerator.Reset();
+            }
+            
+
+        }
+    }
+}
