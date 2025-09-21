667bc8b3aba40a6cfd936cedf3ccd5d469eef25e
PyScriptPlus <mohamadexbier@gmail.com>
2025-09-21 22:27:37 +0330

    Writing code about Convert_ToBase64String with C# in MSVC

---
+++ Learning_CSharp_Full/Tamrin/S3/Base64.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Tamrin.S3
+{
+    internal class Base64
+    {
+        static void B64(string[] args)
+        {
+            Random rnd = new Random();
+
+            byte[] bytess = new byte[128];
+
+            rnd.NextBytes(bytess);
+
+            Console.WriteLine("Binary Object as bytes: ");
+            for (int i = 0; i < bytess.Length; i++)
+            {
+                Console.Write($"{bytess[i]:X2} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            string encode = Convert.ToBase64String(bytess);
+            Console.WriteLine($"Binary Object as Base64: {encode}");
+        }
+    }
+}
