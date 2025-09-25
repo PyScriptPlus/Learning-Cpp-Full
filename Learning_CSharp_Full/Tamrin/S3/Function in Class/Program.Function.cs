49278aca11d55a904ca81310fe953e1d822b4a0f
PyScriptPlus <mohamadexbier@gmail.com>
2025-09-25 20:57:20 +0330

    Writing code about (Function & partial Class) with C# in MSVC

---
+++ Learning_CSharp_Full/Tamrin/S3/Function in Class/Program.Function.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Tamrin.S3.Class
+{
+    partial class Program
+    {
+        // Vaghti in class ro be surat yek partial minevisam
+        // Ya'ni mitunim ghesmat dige sham dar yek file dige 
+        // Benevisim engar ye seri chizaye in class dar in file neveshtim
+        // Va yek seri dige ro toye file Program.Funcion.cs neveshtim
+
+
+        // title is default parameter:
+        void add(int x, int y, string title = "This Number is: ")
+        {
+            Console.WriteLine(title + x + " + " + y + " = " + (x + y));
+        }
+
+        static int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+    }
+}
---
+++ Learning_CSharp_Full/Tamrin/S3/Function in Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Tamrin.S3.Class
+{
+    partial class Program
+    {
+        // Vaghti in class ro be surat yek partial minevisam
+        // Ya'ni mitunim ghesmat dige sham dar yek file dige 
+        // Benevisim engar ye seri chizaye in class dar in file neveshtim
+        // Va yek seri dige ro toye file Program.Funcion.cs neveshtim
+
+        static void Main(string[] args)
+        {
+
+            printHello();
+
+            // In baraye functioni hast ke bedon static hast
+            // Chera new estefade shode? Rezerve hafeze baraye Object
+            Program obj = new Program();
+            obj.add(3, 2);
+
+            // Vali age static bashe bedon new mishe 
+            // Dast rasi peyda kard 
+            Console.WriteLine(multiplication(5, 5)); 
+
+
+            void printHello() // Local Function
+            {
+                Console.WriteLine("Hello");
+                return;
+            }
+        }
+    }
+}
