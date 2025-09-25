2a4b666937a781588c3596e0d470fb39a326a5bf
PyScriptPlus <mohamadexbier@gmail.com>
2025-09-25 20:56:08 +0330

    Writing code about (Nested Exception) with C# in MSVC

---
+++ Learning_CSharp_Full/Tamrin/S3/Inner_Exception.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Tamrin.S3
+{
+    internal class Inner_Exception
+    {
+        static void Nested_Exception()
+        {
+            #region BedonError
+            
+            string fileName = "salam.txt";
+            string writeContect = ("Hello C#\n");
+            try
+            {
+                try
+                {
+                    File.WriteAllText(fileName, writeContect);
+                    string readOnlyTextFile = File.ReadAllText(fileName);
+                    Console.WriteLine(readOnlyTextFile);
+                }
+                catch (Exception)
+                {
+                    throw new Exception($"File: {fileName} peyda nashod!");
+                    //throw new Exception(null);
+                    //Console.WriteLine(msg1.Message);
+                }
+            }
+            catch (Exception msg2)
+            {
+                Console.WriteLine(msg2.Message);
+                Console.WriteLine(msg2.InnerException?.Message);
+            }
+
+            #endregion
+
+            #region Error mide age Null bashe
+            /*
+            string fileName = "dd.txt";
+            //string writeContect = ("Hello C#\n");
+            try
+            {
+                try
+                {
+                    //File.WriteAllText(fileName, writeContect);
+                    string readOnlyTextFile = File.ReadAllText(fileName);
+                    Console.WriteLine(readOnlyTextFile);
+                }
+                catch (Exception)
+                {
+                    //throw new Exception($"File: {fileName} peyda nashod!");
+                    throw new Exception(null);
+                    //Console.WriteLine(msg1.Message);
+                }
+            }
+            catch (Exception msg2)
+            {
+                Console.WriteLine(msg2.Message);
+                Console.WriteLine(msg2.InnerException.Message);
+            }
+            */
+            #endregion
+
+            #region Null pazir kardan OR Null-conditional Operator ( ? )
+            /*
+            string fileName = "dd.txt";
+            //string writeContect = ("Hello C#\n");
+            try
+            {
+                try
+                {
+                    //File.WriteAllText(fileName, writeContect);
+                    string readOnlyTextFile = File.ReadAllText(fileName);
+                    Console.WriteLine(readOnlyTextFile);
+                }
+                catch (Exception)
+                {
+                    //throw new Exception($"File: {fileName} peyda nashod!");
+                    throw new Exception(null);
+                    //Console.WriteLine(msg1.Message);
+                }
+            }
+            catch (Exception msg2)
+            {
+                Console.WriteLine(msg2.Message);
+                Console.WriteLine(msg2.InnerException?.Message);
+            }
+            */
+            #endregion
+
+            Console.ReadLine(); 
+        }
+    }
+}
