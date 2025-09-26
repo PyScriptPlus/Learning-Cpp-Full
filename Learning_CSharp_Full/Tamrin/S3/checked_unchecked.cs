99a79e0fcbb27eba9aaa985d3b01a4abce24f1d1
PyScriptPlus <mohamadexbier@gmail.com>
2025-09-26 15:11:35 +0330

    Writing code about (checked & unchecked) with C# in MSVC

---
+++ Learning_CSharp_Full/Tamrin/S3/checked_unchecked.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Tamrin.S3
+{
+    internal class checked_unchecked
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                    int overflow = int.MaxValue;
+                    int overflow2 = int.MinValue;
+                    ++overflow;
+                    ++overflow;
+                    --overflow2;
+                    --overflow2;
+                }
+            }
+            catch(OverflowException msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+            
+            // Check nemikone
+            unchecked
+            {
+                int overflow = int.MaxValue;
+                int overflow2 = int.MinValue;
+                ++overflow;
+                ++overflow;
+                --overflow2;
+                --overflow2;
+            }
+        }
+    }
+}
