75034bcd70d29045101955426eeca07b30a79650
PyScriptPlus <mohamadexbier@gmail.com>
2025-10-02 14:12:20 +0330

    Writing code about (Recursion_Function) and Trace with C# in MSVC

---
+++ Learning_CSharp_Full/Tamrin/S3/Factorial_Recursion/Recursion_Function.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Tamrin.S3.Factorial_Recursion
+{
+    internal class Recursion_Function
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(factorial(5));
+        }
+
+        static int factorial(int x)
+        {
+            if(x == 0)
+            {
+                return 1;
+            }
+            return factorial(x - 1) * x;
+        }
+
+    }
+}
