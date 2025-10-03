8ca678d50b81fff9aa65af4a3635ab0682d40609
PyScriptPlus <mohamadexbier@gmail.com>
2025-10-03 22:59:32 +0330

    Writing code about (Params Keyword) with C# in MSVC

---
+++ Learning_CSharp_Full/Tamrin/S3/Params_Keyword.cs
@@ -0,0 +1,29 @@
+﻿using Learning_CSharp_Full.Tamrin.S3.Factorial_Recursion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Tamrin.S3
+{
+    internal class Params_Keyword
+    {
+        static void arrays(params int[] numbers)
+        {
+            int x = 0;
+            while(x < numbers.Length)
+            {
+                Console.WriteLine(numbers[x]);
+                x++;
+            }
+            return;
+        }
+        static void Main()
+        {
+            // In moshakhas nist chegard tul dare in Parameter
+            // params baraye hamin estefade mishe
+            arrays(5, 5, 5, 5, 323,11);
+        }
+    }
+}
