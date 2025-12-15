8774e5909b906d4bc54ff60f047a8909913b2613
PyScriptPlus <mohamadexbier@gmail.com>
2025-12-15 20:50:33 +0330

    Writing code about Fields with C#

---
+++ Learning_CSharp_Full/All Field/Fields.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.All_Field
+{
+    public class Fields
+    {
+        public const string? fname = "Mamad"; // const field
+        public readonly string lname = "Cplus-Cplus"; // readonly field
+        // Faghat dar zaman Constructor ya sakht instance 
+        // Meghdar dahi mishe ba'ad az ina nemishe das zad
+
+        public Fields()
+        {
+            lname = "Yousefi";
+        }
+
+        public required int rand; // Requiring fields
+        // Henghame sakht Instance hatman bayad meghdar dahi kard
+        // Vagarne error mide !!! ejbarie
+        // Ba'ad az sakht Instance mishe taghir dad
+
+    }
+}
---
+++ Learning_CSharp_Full/All Field/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.All_Field
+{
+    public class Program
+    {
+        static void lsls()
+        {
+            Fields fl = new()
+            {
+                rand = 50
+            };
+
+        }
+    }
+}
