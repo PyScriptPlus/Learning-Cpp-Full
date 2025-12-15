3298933addf1842ae4da8b732b5ab1c98c62da91
PyScriptPlus <mohamadexbier@gmail.com>
2025-12-15 20:50:52 +0330

    Writing code about List with Class with C#

---
+++ Learning_CSharp_Full/List with class/List.cs
@@ -0,0 +1,33 @@
+﻿using Learning_CSharp_Full.List_with_class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full
+{
+    public class List
+    {
+        static void Listt()
+        {
+            Random rnd = new();
+            Person2 dsd = new();
+            int y = default;
+            for (int i = 0; i <= rnd.Next(); i++)
+            {
+                ++y;
+            }
+
+            List<Person2> pr = new();
+
+            pr.Add(new Person2 { number =  y});
+            for (int j = 0; j < pr.Count; j++)
+            {
+                Console.WriteLine(pr[j].number);
+            }
+
+
+        }
+    }
+}
---
+++ Learning_CSharp_Full/List with class/Person2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.List_with_class
+{
+    public class Person2
+    {
+        public int number;
+    }
+}
