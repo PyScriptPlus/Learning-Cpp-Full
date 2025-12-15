6b13240df34124acb68dac46041897890f178183
PyScriptPlus <mohamadexbier@gmail.com>
2025-12-15 20:52:09 +0330

    Writing code about object initializer syntax with C#

---
+++ Learning_CSharp_Full/object initializer syntax/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.object_initializer_syntax
+{
+    public class Person
+    {
+        public string fName { get; set; }
+        public int age { get; set; }
+        public DateTime born;
+    }
+}
---
+++ Learning_CSharp_Full/object initializer syntax/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.object_initializer_syntax
+{
+    public class Program
+    {
+        static void OBJ()
+        {
+            Person person = new()
+            {
+                fName = "Mamad",
+                age = 18,
+                born = new(2008,3,24)
+            };
+            Console.WriteLine("Your name is: " + person.fName);
+            Console.WriteLine("Your age is: {0} and born: {1}"
+            , person.age,person.born);
+
+        }
+    }
+}
