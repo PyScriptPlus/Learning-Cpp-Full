c4999a8a7197450ed6b26857bbfc34d97950719f
PyScriptPlus <mohamadexbier@gmail.com>
2025-12-15 20:51:43 +0330

    Writing code about Defining multiple constructors with C#

---
+++ Learning_CSharp_Full/Defining multiple constructors/Constructor_multiple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Defining_multiple_constructors
+{
+    public class Constructor_multiple
+    {
+        public string fname;
+        public string lname;
+        public int age;
+        public DateTime born;
+
+        public Constructor_multiple
+        (string Firstname, string Lastname, int _age, DateTime _born)
+        {
+            fname = Firstname;
+            lname = Lastname;
+            age = _age;
+            born = _born;
+        }
+        public void printStatus()
+        {
+            Console.WriteLine("Your name is: {0} and lname: {1}",
+                fname,lname);
+            Console.WriteLine("Your age is: {0}", age);
+            Console.WriteLine("Your born is: {0}", born);
+        }
+    }
+}
---
+++ Learning_CSharp_Full/Defining multiple constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_Full.Defining_multiple_constructors
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string fname = "Mohammad";
+            string lname = "Yousefi";
+            int age = 18;
+
+
+            Constructor_multiple _Multiple =
+                new(fname, lname, age, new(2008, 3, 14));
+            _Multiple.printStatus();
+        }
+    }
+}
