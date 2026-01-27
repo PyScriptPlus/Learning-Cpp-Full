c82c58f0049b72c0f729b2dee2c931a9c3c9abc5
PyScriptPlus <mohamadexbier@gmail.com>
2026-01-28 00:41:54 +0330

    obj file & log (ziad mohem nist)!

--- DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/obj/Debug/net8.0/DataType_cs.AssemblyInfo.cs
+++ DataType C++, C# Visual Studio 2022/tester1C/DataType_cs/obj/Debug/net8.0/DataType_cs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7eefb1c23b544a1876d0db07a621cbd7153bac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3047f8490ccc22ac5145b215079d8fbdec208fad")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataType_cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
