16d103d9691eef2cd9514562a90f6a7ae67fdacb
PyScriptPlus <mohamadexbier@gmail.com>
2026-04-26 23:53:41 +0330

    Explanations about software testing

---
+++ CodeVault/CodeVault/CodeVault/SoftwareTesting.cpp
@@ -0,0 +1,49 @@
+// Aya vaghti ma yek barname minevisim va dorust kar mikone kar tamume?
+// Bastegi dare agar ma barname ro neveshtim faghat 1 martabe ejra beshe
+// Ba'ad kenar begozarim bale kar to tamume !!!
+// Barnamei benevisi ke Linear bashe karet tamume
+
+// Manzur az Linear chiye? Bedun if, while va ...
+// Ya'ni barname aslan hich vorudi daryaft nemikone
+
+// Vali barname darim tosh az loop estefade shode
+// Kar tamum nist chera? Chon inke barname yek bar dorust amal karde
+// Taht 1 halat bude va in hich tazmini be ma nemide ke dar sayer halat
+// Ha ham be dorusti kar kone !!!
+
+// Bahs Sofwate Testing ham baraye in mas'ale shekl gerft
+// Karesh ineke tayin kone aya yek barname dar sayer halat be dorusti
+// Kar mikone ya na.
+// Hich testi vojud nadare va aslan ham hadaf test in nist ke tamam
+// Kalam halat barname ro test kone
+
+// Mesal mizanam khob tavvajoh kon:
+// Farz kon 2 ta Variable darim x, y beheshun meghdar midim
+// auto x = 5, y = 4;
+// Dar if yek condition anjam midim kodum bozorg tare 
+// Age javab x bod --> Halat 1 dorust bod.
+// Test dige baraye less than va equel ham mizanim
+// Age in halat ha ham javab dad va dorust bod
+// Pas barnamamun doruste
+// Baraye chenin chizi Test minevisim
+// Ke in 3 Halat ke bala goftam be dorusti kar mikonanad ya na
+
+// Shayad tarz fekret az test in bod ke miad
+// Hameye adad haro yeki yeki test mikone
+// Shayad 50000 bozorg tar az 1 hast na injuri nist
+// Nemiyad hameye maghadir x, y ro test kone
+
+
+// Estelah haye: Unit Test, Integration Test
+
+// Unit Test: Yek bakhsh kochaki az code ro be surat
+// Isolate Ya'ni (joda) az baname test mikone.
+
+// Integration Test: Zamani ke ma Unit Test hamun ro be sorat jodagone
+// Test kardim Unit haro kenar ham gharar midim va baham test mikonimeshun
+
+// In test ha ba yek seri Tools haye makhsus har zaban anjam midim:
+// Baraye zaban C --> Google Test, cunit, check
+// Baraye zaban C++ --> Google Test
+// Baraye zaban C#  --> XUnit
+// Baraye zaban Java --> Junit
