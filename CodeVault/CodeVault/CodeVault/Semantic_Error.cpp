7114c28763aa8193d0a18e2d7ff4860e99e0ba58
PyScriptPlus <mohamadexbier@gmail.com>
2026-04-26 23:55:44 +0330

    Explanations about Semantic Error in C++

---
+++ CodeVault/CodeVault/CodeVault/Semantic_Error.cpp
@@ -0,0 +1,27 @@
+// Dar gozashte dar mored Syntax Error sohbar kardim
+// Goftim in noe' error ha zamani be vojud biyad ke
+// Grammer zaban ro ro'ayat nakonim va khod Compiler ham
+// Mige khata koja surat gerfte
+
+// Dar noghte moghabel ma
+// Semantic Error haro darim ke bar taraf kardaneshun sakhte
+// Chon osulan ma codi khalaf ghave'd naneveshtim
+// Vali barname kari ke azash entezar dashtim bekone ro
+// Nemikone Semantic Error momken monjar be UB beshe
+
+// Undefined Behavior chi bod: Barname natayej ghalati
+// Tolid bekone, Monjar mishod yek seri oghat natije eshtebah
+// Bashe, Ba'es Crash shodan barname mishe
+
+// Mesal: Barname neveshtam adad 10 ro ba 5 zarb mikone
+// Be jaye adad 50 dar javab 2 ro neshun mide
+// Bejaye * omadam / gozashtam
+
+// Dar project haye bozorg peyda kardan chenin error hayi
+// Ehtemal chand sa'at ya hata chand ruz tul bekeshad
+
+// Vizheghi haye Semantic Error:
+// 1- Compiler ha ma'molan on ra tashkhis nemidahad.
+// 2- Barname ejra mishavad.
+// 3- Khoruji ya raftar barname eshtebah hast.
+// 4- Marbut be mantegh (Logic) barname ast.
